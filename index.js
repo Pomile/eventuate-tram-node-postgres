d65f5bbf37064c513c6c627a46f5a7ce376eb113
Alex <soft-sky@live.co.uk>
2023-06-29 17:57:44 +0100

    update index file

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const MessageConsumer = require('./lib/kafka/MessageConsumer');
 const SwimlaneDispatcher = require('./lib/kafka/SwimlaneDispatcher');
 const ObservableQueue = require('./lib/ObservableQueue');
 const KafkaProducer = require('./lib/kafka/KafkaProducer');
+const dotenv = require('dotenv')
+const { getLogger } = require('./lib/utils');
+const knex = require('./lib/postgres/knex');
+const logger = getLogger({ title: 'eventuateConnection' });
+dotenv.config();
+
+
+(async () => {
+  try {
+    logger.info(`Host: ${process.env.EVENTUATE_TRAM_POSTGRES_HOST}`)
+    await knex.raw('SELECT 1').then(() => {
+      
+      logger.info('Connection successful')
+    }).catch(err => {
+      logger.info('Connection failed', err)
+    });
+  } catch(err) {
+    logger.info('Connection failed', err)
+  }
+})()
+
 
 module.exports = {
   KafkaProducer,
